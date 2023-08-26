dcfbaa80f73bb369363bb37b8fd78018c8d51fcf
roshan <roshan.net>
2023-08-27 01:20:25 +0530

    update

--- src/component/home.js
+++ src/component/home.js
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import {Grid } from "@mui/material"
 export default function Home() {
   return (
     <div>
       <h1>dsfdsfdsffs</h1>
+      <Grid>
+        kmdlsfkfmdslk
+      </Grid>
     </div>
   )
 }
