a380180c894a1ae3be5be6e52100c89e5473a79e
RoSHan0008 <103057578+RoSHan0008@users.noreply.github.com>
2023-09-14 14:49:01 +0000

    Updates

--- src/Redux/Store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { createReducers } from "./Reducer";
-
-const store = configureStore({
-    reducer:{ 
-        custom: createReducers
-    }
-}) 
-export default store
